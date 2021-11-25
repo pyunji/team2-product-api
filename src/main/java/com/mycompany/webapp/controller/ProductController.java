a365c36856555f6ed7fe4c924274d003f51eeaf2
eunsol <a2670624@naver.com>
2021-11-25 16:44:32 +0900

    상품 카테고리별 조회 API

--- src/main/java/com/mycompany/webapp/controller/ProductController.java
+++ src/main/java/com/mycompany/webapp/controller/ProductController.java
@@ -1,9 +1,11 @@
 package com.mycompany.webapp.controller;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mycompany.webapp.dto.ProductList;
+import com.mycompany.webapp.dto.ProductListView;
 import com.mycompany.webapp.service.ProductService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -32,6 +35,14 @@ public class ProductController {
 	public List<ProductList> getProductList(@RequestParam String d1name) {
 		log.info("getProductList");
 		List<ProductList> products = productService.getProductSampleList(d1name);
+		log.info("products = {}", products);
+		return products;
+	}
+	
+	@GetMapping("/lists")
+	public List<ProductListView> getProductListByCategory(@RequestParam String d1name,@RequestParam(required=false) String d2name,@RequestParam(required=false) String d3name) {
+		log.info("getProductListByCategory");
+		List<ProductListView> products = productService.getProductListByCategory(d1name,d2name,d3name);
 		log.info("products = {}", products);
 		return products;
 	}
---
+++ src/main/java/com/mycompany/webapp/dto/Product.java
@@ -0,0 +1,16 @@
+package com.mycompany.webapp.dto;
+
+import lombok.Data;
+
+@Data
+public class Product{
+	String pcommonid;
+	String pname;
+	String bname;
+	String img1;
+	String color_img;
+	int pprice;
+	String d1name;
+	String d2name;
+	String d3name;
+}
